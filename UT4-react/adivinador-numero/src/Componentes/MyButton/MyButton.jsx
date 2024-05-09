d00315794b43da5c7ebd36aa6dea186a3c6634db
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2024-05-08 22:33:27 -0300

    Adivinador

---
+++ UT4-react/adivinador-numero/src/Componentes/MyButton/MyButton.jsx
@@ -0,0 +1,10 @@
+function MyButton({ color, click, label }) {
+    return (
+      <button className={color} onClick={click}>
+        {" "}
+        {label}
+      </button>
+    );
+  }
+  
+  export default MyButton;
